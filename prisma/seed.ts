c191d2de4e1e071587e05167c8db940567fddd37
Martial <floquet.martial@gmail.com>
2026-01-15 11:32:43 +0100

    feat(db): update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-
+import { useTranslations } from "next-intl";
 const prisma = new PrismaClient();
 
 async function main() {
+  const t = useTranslations();
   const users = [
     {
       email: "marti@example.com",
@@ -107,17 +108,17 @@ async function main() {
   const activities = [
     {
       author: "Léa",
-      action: "a créé le ticket MYJ-214",
+      action: "a créé le ticket MYJ-214" + " " + t("todo"),
       timestamp: new Date(Date.now() - 5 * 60 * 1000),
     },
     {
       author: "Victor",
-      action: "a déplacé MYJ-198 vers En cours",
+      action: "a déplacé MYJ-198 vers " + " " + t("inProgress"),
       timestamp: new Date(Date.now() - 32 * 60 * 1000),
     },
     {
       author: "Yasmine",
-      action: "a clôturé MYJ-176",
+      action: "a clôturé MYJ-176" + " " + t("done"),
       timestamp: new Date(Date.now() - 60 * 60 * 1000),
     },
   ];
