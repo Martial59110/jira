d640abd45d944738ac8533ca3ca3a2065ec16bd7
Martial <floquet.martial@gmail.com>
2026-01-15 11:30:27 +0100

    feat(i18n): export new translation namespaces

---
+++ app/messages/en/index.ts
@@ -0,0 +1,29 @@
+import ActivityList from "./ActivityList.json";
+import BoardGrip from "./BoardGrip.json";
+import BoardHeader from "./BoardHeader.json";
+import KanbanColumn from "./KanbanColumn.json";
+import CreateIssueForm from "./CreateIssueForm.json";
+import CreateIssueModal from "./CreateIssueModal.json";
+import AppNav from "./AppNav.json";
+import Login from "./Login.json";
+import SummaryCards from "./SummaryCards.json";
+import Dashboard from "./Dashboard.json";
+import DashboardError from "./DashboardError.json";
+import Activity from "./Activity.json";
+import Register from "./Register.json";
+
+export const en = {
+    ActivityList,
+    BoardGrip,
+    KanbanColumn,
+    BoardHeader,
+    CreateIssueForm,
+    CreateIssueModal,
+    AppNav,
+    Login,
+    SummaryCards,
+    Dashboard,
+    DashboardError,
+    Activity,
+    Register
+}
